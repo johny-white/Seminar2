5d597c59daf3c24ea648d1b5922e72f380c32a78
white <white-johny@mail.ru>
2023-11-25 16:22:10 +0300

    add Zadacha4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int a1=a;
+int n=1;
+while (a1>=10)
+{
+    a1/=10;
+    n*=10;
+}
+while (n>1)
+{
+   Console.Write(a/n%10 +", ");
+   n/=10;
+ }
+Console.WriteLine(a%10);
